6b12aef801fe50d3c80eff0c426a70a702649a1c
ansari-Bilal <144584066+ansari-Bilal@users.noreply.github.com>
2024-01-03 15:26:42 +0000

    Conversion of datatypes

---
+++ Basics/con.js
@@ -0,0 +1,20 @@
+// let score= "50"
+// score= "50abc"
+
+// let color = "Blue"
+
+let abcd= null
+let xyzabc;
+
+console.log(typeof xyzabc);
+console.log(typeof (xyzabc));  // We can write in parenthesis also
+
+let value= Number(xyzabc)  //type casting to number type
+
+console.log(typeof value);
+console.log(value);  
+  
+
+
+//abdnKJWNOWNVOKVLMOKvl, l,c 
+//NaN = Not a Number 
