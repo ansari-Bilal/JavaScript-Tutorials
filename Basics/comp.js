272c726dcab5b0377c24a3dba5a187b486e6617a
ansari-Bilal <144584066+ansari-Bilal@users.noreply.github.com>
2024-01-03 19:40:39 +0000

    Comparisons of dataTypes in JS

---
+++ Basics/comp.js
@@ -0,0 +1,14 @@
+// console.log(2 > 1);
+
+// console.log(null == 0);
+// console.log(null > 0);
+// console.log(null > 0);
+// console.log(null <= 0);
+// console.log(null >= 0);
+
+// console.log(true == 0);
+
+// console.log(undefined == 0);
+
+console.log("0" == 0 );  //=> true -->bcoz only values are compared
+console.log("0" === 0);  //=>false -->it is strictly equal where values as well as the dataypesare also compared
